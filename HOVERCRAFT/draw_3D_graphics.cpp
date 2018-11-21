1faab052a8d4f8b9e6952d5ee0c20859e814ab05
Hugo Provencher <hugo1507@msn.com>
2018-11-21 15:16:59 -0500

    Update draw_3D_graphics.cpp
    added first person view

--- HOVERCRAFT/draw_3D_graphics.cpp
+++ HOVERCRAFT/draw_3D_graphics.cpp
@@ -77,7 +77,7 @@ public:
 
 	void input();// change the inputs to Fl or Fr to 1 or 0;
 
-
+	double get_xy(int index);
 
 };
 
@@ -87,7 +87,7 @@ Hovercraft::Hovercraft(double y[8], char *file_name) {
 
 	c1 = 0.5; //Drag coefficients 0.5
 	c2 = 0.5;
-	c3 = 0.000001; // So it doesn't spin forever.
+	c3 = 0.0000000000000001; // So it doesn't spin forever.
 	J = 1; // inertia 1
 	L = 1; // 1 m between motors
 	Mass = 1; // mass 1
@@ -113,7 +113,6 @@ Hovercraft::~Hovercraft(){
 
 
 
-
 void Hovercraft::draw(){ //draw the hover craft
 
 	p_mesh->Scale = 0.3;
@@ -134,15 +133,39 @@ void Hovercraft::sim_step(double dt){
 	}
 }
 
+double Hovercraft::get_xy(int index){
+	return X[index];
+}
+
 void Hovercraft::input(){
 	{
 	
 
 		
-		/*if (KEY(VK_UP)) {
+		if (KEY(VK_UP)) {
+			// void set_view(double *eye_point, double *lookat_point, double *up_dir, double fov=3.14159/4);
 			
-		}*/
-		//Commented this out because not used.
+			double eye_point[4], lookat_point[4], up_dir[4]; // 3+1 because the prof's library starts at index 1.
+
+
+			eye_point[1] = 0.0 + get_xy(6);  // x
+			eye_point[2] = 0.0 + get_xy(7); // y
+			eye_point[3] = 0.0; // z
+
+
+
+			// the position you are looking at in global coord
+			lookat_point[1] = 0.0 + get_xy(6) + 2 * cos(get_xy(4)); // x
+			lookat_point[2] = 0.0 + get_xy(7) + 2 * sin(get_xy(4)); // y
+			lookat_point[3] = 1.0; // z
+
+			// the direction of the top of your head (note: up is a direction, not a point)
+			up_dir[1] = cos(get_xy(4)); // dx
+			up_dir[2] = sin(get_xy(4)); // dy 
+			up_dir[3] = 0.0;;//dz
+
+			set_view(eye_point, lookat_point, up_dir); // fpv when up is pressed.
+		}
 
 		//click down arrow key to reset position *** Change it if you can 
 		if (KEY(VK_DOWN)) {
@@ -153,13 +176,13 @@ void Hovercraft::input(){
 
 		// Fr goes to 1 so eulers function works
 		if (KEY(VK_RIGHT)){
-			U[1] = 1.0;
+			U[0] = 1.0;
 		}
 		else if (KEY(0x0058)){ // Press X to have the motor work in reverse
-			U[1] = -1.0;
+			U[0] = -1.0;
 		}
 		else{
-			U[1] = 0.0;
+			U[0] = 0.0;
 		} 
 			
 			
@@ -167,13 +190,13 @@ void Hovercraft::input(){
 
 		// Fl goes to 1
 		if (KEY(VK_LEFT)){
-			U[0] = 1.0;
+			U[1] = 1.0;
 		}
 		else if (KEY(0x005A)){ // Press Z to have the motor work in reverse
-			U[0] = -1.0;
+			U[1] = -1.0;
 		}
 		else{
-			U[0] = 0.0;
+			U[1] = 0.0;
 		}
 		
 
@@ -218,7 +241,27 @@ void draw_3D_graphics(){
 
 	static Hovercraft H1(X, file_name);
 
-	set_view();
+	//set_view();
+
+	double eye_point[4], lookat_point[4], up_dir[4]; // 3+1 because the prof's library starts at index 1.
+	eye_point[1] = 0.0 + H1.get_xy(6);  // x
+	eye_point[2] = 0.0 + H1.get_xy(7); // y
+	eye_point[3] = 5.0; // z
+
+
+
+	// the position you are looking at in global coord
+	lookat_point[1] = 0.0 + H1.get_xy(6); // x
+	lookat_point[2] = 0.0 + H1.get_xy(7); // y
+	lookat_point[3] = 0.0; // z
+
+	// the direction of the top of your head (note: up is a direction, not a point)
+	up_dir[1] = 1.0; // dx
+	up_dir[2] = 0.0; // dy 
+	up_dir[3] = 0.0;;//dz
+
+	set_view(eye_point, lookat_point, up_dir);
+	//draw_XYZ(100.0);
 	draw_XYZ(5.0);
 
 	H1.eulers();
