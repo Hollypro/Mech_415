ddefef464b1e0adad9248150d7c7c86720d923d2
Hugo Provencher <hugo1507@msn.com>
2018-12-14 00:34:59 -0500

    Update draw_3D_graphics.cpp

--- HOVERCRAFT/draw_3D_graphics.cpp
+++ HOVERCRAFT/draw_3D_graphics.cpp
@@ -69,6 +69,16 @@ class Hovercraft {
 
 	double Tar_Pos[2];
 
+	double Fol_Stat_V[3];
+	//index# : 0 1 2
+	//StateV : x y yaw
+
+	double Fol_DStatV[3];
+	//index# : 0 1 2
+	//StateV : u v r
+
+	double Fol_Velocity;
+
 public:
 
 	
@@ -85,6 +95,7 @@ public:
 	mesh *p_env;
 	mesh *p_bull;
 	mesh *p_tar;
+	mesh *p_fol;
 
 	Hovercraft(double *X, char *file_name);//constructor
 
@@ -135,8 +146,6 @@ Hovercraft::Hovercraft(double y[8], char *file_name) {
 	BStat_Var[3] = X[3];
 	BStat_Var[4] = 0;
 	BStat_Var[5] = 0;
-	BDStatV[0] = cos(BStat_Var[4]) * Velocity;
-	BDStatV[1] = sin(BStat_Var[4]) * Velocity;
 
 	Velocity = 10;
 
@@ -147,6 +156,14 @@ Hovercraft::Hovercraft(double y[8], char *file_name) {
 	BDStatV[4] = 0;
 	BDStatV[5] = 0;
 
+	Fol_Stat_V[0] = 0.0;
+	Fol_Stat_V[1] = -0.5;
+	Fol_Stat_V[2] = 0.0;
+
+	Fol_DStatV[0] = 0.0;
+	Fol_DStatV[1] = 0.0;
+	Fol_DStatV[2] = 0.0;
+
 	srand(time(NULL)); //Randomly place the target within the map.
 	Tar_Pos[0] = rand() % 70 - 35;
 	srand(time(NULL));
@@ -156,12 +173,17 @@ Hovercraft::Hovercraft(double y[8], char *file_name) {
 	p_env = new mesh("track2.x");
 	p_bull = new mesh("Bullet.x");
 	p_tar = new mesh("Target.x");
+	p_fol = new mesh(file_name);
 }
 
 Hovercraft::~Hovercraft(){
 	p_mesh = nullptr;
 	p_env = nullptr;
 	p_bull = nullptr;
+	p_tar = nullptr;
+	p_fol = nullptr;
+	delete p_tar;
+	delete p_fol;
 	delete p_bull;
 	delete p_env;
 	delete p_mesh;
@@ -176,19 +198,12 @@ void Hovercraft::draw(){ //draw the hover craft
 	p_env->Scale = 0.05;
 	p_bull->Scale = 0.005;
 	p_tar->Scale = 0.25;
+	p_fol->Scale = 0.3 * 0.5;
 	p_env->draw(0.0, 0.0, 1.5, 0.0, 0.0, 0.0);
-
-	double dist = sqrt((BStat_Var[0] - Tar_Pos[0]) * (BStat_Var[0] - Tar_Pos[0]) + (BStat_Var[1] - Tar_Pos[1])*  (BStat_Var[1] - Tar_Pos[1]));
-
-	if (dist < 0.5){
-		srand(time(NULL));
-		Tar_Pos[0] = rand() % 80 - 40;
-		srand(time(NULL));
-		Tar_Pos[1] = rand() % 30 - 15;
-	}
 
 	// void draw(double Tx, double Ty, double Tz, double yaw, double pitch, double roll);
 	p_mesh->draw(X[6], X[7], 0.0, X[4] + PI, 0.0, PI / 2);
+	p_fol->draw(Fol_Stat_V[0], Fol_Stat_V[1], 0.0, Fol_Stat_V[2] + PI, 0.0, PI / 2);
 	if (View){
 		p_bull->draw(BStat_Var[0], BStat_Var[1], 0.0, BStat_Var[4] + PI / 2, 0.0, PI);
 		// void draw(double Tx, double Ty, double Tz, double yaw, double pitch, double roll);
@@ -215,6 +230,9 @@ void Hovercraft::sim_step(double dt){
 	for (int i = 0; i < 6; i++){
 		BStat_Var[i] += BDStatV[i] * dt;
 	}
+	for (int i = 0; i < 3; i++){
+		Fol_Stat_V[i] += Fol_DStatV[i] * dt;
+	}
 }
 
 double Hovercraft::get_xy(int index){
@@ -226,110 +244,124 @@ double Hovercraft::get_Bullxy(int index){
 }
 
 void Hovercraft::input(){
-	{
-		//auto Then = std::chrono::duration<double>(TimeThen.time_since_epoch());
-		//auto TimeNow = chrono::high_resolution_clock::now();					
-		//auto Now = std::chrono::duration<double>(TimeNow.time_since_epoch());	
+	//auto Then = std::chrono::duration<double>(TimeThen.time_since_epoch());
+	//auto TimeNow = chrono::high_resolution_clock::now();					
+	//auto Now = std::chrono::duration<double>(TimeNow.time_since_epoch());	
 
-		/*This whole section is to ensure enough time has passed since switching between third person
-		view and fps, to prevent me from puking while debugging.*/
+	/*This whole section is to ensure enough time has passed since switching between third person
+	view and fps, to prevent me from puking while debugging.*/
 
-		/*It turns out that VS 2013 is known for being unable to deal with time.
-		https://stackoverflow.com/questions/17769172/non-conforming-return-value-for-stdchronodurationoperator-in-microsoft
-		https://stackoverflow.com/questions/24586804/c11-chrono-in-visual-studio-2013
-		*/
-
-
-		//double TimeSince = Now.count() - Then.count();			
-
-		if (KEY(VK_UP) && !(Prev_Key) ){
-			View = !View;
-			Prev_Key = true;
-			//auto TimeThen = chrono::high_resolution_clock::now();
-		}
-		else if (!(KEY(VK_UP))){ //So that holding the up key won't flash between the two views.
-			Prev_Key = false;
-		}
-
-		if (View){}
-		else{
-			// void set_view(double *eye_point, double *lookat_point, double *up_dir, double fov=3.14159/4);
-			
-			double eye_point[4], lookat_point[4], up_dir[4]; // 3+1 because the prof's library starts at index 1.
+	/*It turns out that VS 2013 is known for being unable to deal with time.
+	https://stackoverflow.com/questions/17769172/non-conforming-return-value-for-stdchronodurationoperator-in-microsoft
+	https://stackoverflow.com/questions/24586804/c11-chrono-in-visual-studio-2013
+	*/
 
 
-			eye_point[1] = 0.0 + 1 * get_xy(6);  // x
-			eye_point[2] = 0.0 + 1 * get_xy(7); // y
-			eye_point[3] = -0.3; // z
+	//double TimeSince = Now.count() - Then.count();			
+
+	if (KEY(VK_UP) && !(Prev_Key)){
+		View = !View;
+		Prev_Key = true;
+		//auto TimeThen = chrono::high_resolution_clock::now();
+	}
+	else if (!(KEY(VK_UP))){ //So that holding the up key won't flash between the two views.
+		Prev_Key = false;
+	}
+
+	if (View){}
+	else{
+		// void set_view(double *eye_point, double *lookat_point, double *up_dir, double fov=3.14159/4);
+
+		double eye_point[4], lookat_point[4], up_dir[4]; // 3+1 because the prof's library starts at index 1.
+
+
+		eye_point[1] = 0.0 + 1 * get_xy(6);  // x
+		eye_point[2] = 0.0 + 1 * get_xy(7); // y
+		eye_point[3] = -0.3; // z
 
 
 
-			// the position you are looking at in global coord
-			lookat_point[1] = 0.0 + 1 * get_xy(6) + 2 * cos(get_xy(4)); // x
-			lookat_point[2] = 0.0 + 1 * get_xy(7) + 2 * sin(get_xy(4)); // y
-			lookat_point[3] = -1.0; // z
+		// the position you are looking at in global coord
+		lookat_point[1] = 0.0 + 1 * get_xy(6) + 2 * cos(get_xy(4)); // x
+		lookat_point[2] = 0.0 + 1 * get_xy(7) + 2 * sin(get_xy(4)); // y
+		lookat_point[3] = -1.0; // z
 
-			// the direction of the top of your head (note: up is a direction, not a point)
-			up_dir[1] = 0.0;//sin(get_xy(4)); // dx
-			up_dir[2] = 0.0;//cos(get_xy(4)); // dy 
-			up_dir[3] = 1.0;;//dz
+		// the direction of the top of your head (note: up is a direction, not a point)
+		up_dir[1] = 0.0;//sin(get_xy(4)); // dx
+		up_dir[2] = 0.0;//cos(get_xy(4)); // dy 
+		up_dir[3] = 1.0;;//dz
 
-			set_view(eye_point, lookat_point, up_dir); // fpv when up is pressed.
-		}
+		set_view(eye_point, lookat_point, up_dir); // fpv when up is pressed.
+	}
 
-		//click down arrow key to reset position *** Change it if you can 
-		if (KEY(VK_DOWN)) {
-			for (int i = 0; i < 8; i++) {
-				X[i] = { 0.0 };
-			}
-		}
-
-		// Fr goes to 1 so eulers function works
-		if (KEY(VK_RIGHT)){
-			U[0] = 1.0;
-		}
-		else if (KEY(0x0058)){ // Press X to have the motor work in reverse
-			U[0] = -1.0;
-		}
-		else{
-			U[0] = 0.0;
-		} 
-			
-			
-		
-
-		// Fl goes to 1
-		if (KEY(VK_LEFT)){
-			U[1] = 1.0;
-		}
-		else if (KEY(0x005A)){ // Press Z to have the motor work in reverse
-			U[1] = -1.0;
-		}
-		else{
-			U[1] = 0.0;
-		}
-		
-		if (KEY(VK_SPACE)){
-
-			Velocity = 10 + sqrt(X[1] * X[1] + X[3] * X[3]);
-			//Hovercraft State Variable
-			//index# : 0  1 2  3 4   5 6  7
-			//StateV : xb u yb v yaw r xc yc
-
-			//Bullet State variables
-			// index 0 1 2 3     4   5
-			// Var:  x y z pitch yaw roll
-			BStat_Var[0] = X[6];
-			BStat_Var[1] = X[7];
-			BStat_Var[2] = 0;
-			BStat_Var[3] = X[3];
-			BStat_Var[4] = X[4];
-			BStat_Var[5] = 0;
-			BDStatV[0] = cos(BStat_Var[4]) * Velocity;
-			BDStatV[1] = sin(BStat_Var[4]) * Velocity;
+	//click down arrow key to reset position *** Change it if you can 
+	if (KEY(VK_DOWN)) {
+		for (int i = 0; i < 8; i++) {
+			X[i] = { 0.0 };
 		}
 	}
+
+	// Fr goes to 1 so eulers function works
+	if (KEY(VK_RIGHT)){
+		U[0] = 1.0;
+	}
+	else if (KEY(0x0058)){ // Press X to have the motor work in reverse
+		U[0] = -1.0;
+	}
+	else{
+		U[0] = 0.0;
+	}
+
+
+
+
+	// Fl goes to 1
+	if (KEY(VK_LEFT)){
+		U[1] = 1.0;
+	}
+	else if (KEY(0x005A)){ // Press Z to have the motor work in reverse
+		U[1] = -1.0;
+	}
+	else{
+		U[1] = 0.0;
+	}
+
+	if (KEY(VK_SPACE)){
+
+		Velocity = 10 + sqrt(X[1] * X[1] + X[3] * X[3]);
+		//Hovercraft State Variable
+		//index# : 0  1 2  3 4   5 6  7
+		//StateV : xb u yb v yaw r xc yc
+
+		//Bullet State variables
+		// index 0 1 2 3     4   5
+		// Var:  x y z pitch yaw roll
+		BStat_Var[0] = X[6];
+		BStat_Var[1] = X[7];
+		BStat_Var[2] = 0;
+		BStat_Var[3] = X[3];
+		BStat_Var[4] = X[4];
+		BStat_Var[5] = 0;
+		BDStatV[0] = cos(BStat_Var[4]) * Velocity;
+		BDStatV[1] = sin(BStat_Var[4]) * Velocity;
+	}
+
+
+	double dist = sqrt((BStat_Var[0] - Tar_Pos[0]) * (BStat_Var[0] - Tar_Pos[0]) + (BStat_Var[1] - Tar_Pos[1])*  (BStat_Var[1] - Tar_Pos[1]));
+
+	if (dist < 0.5){
+		srand(time(NULL));
+		Tar_Pos[0] = rand() % 80 - 40;
+		srand(time(NULL));
+		Tar_Pos[1] = rand() % 30 - 15;
+	}
+
+	Fol_Velocity = sqrt((Fol_Stat_V[0] - X[6]) * (Fol_Stat_V[0] - X[6]) + (Fol_Stat_V[1] - X[7])*  (Fol_Stat_V[1] - X[7])) * 10.0;
+	BDStatV[0] = Fol_Velocity * (Fol_Stat_V[0] - X[6]);
+	BDStatV[1] = Fol_Velocity * (Fol_Stat_V[1] - X[7]);
+	BDStatV[2] = Fol_Velocity * (Fol_Stat_V[2] - X[4]);
 }
+
 
 void Hovercraft::eulers(){
 	// All of the following equations are either from the hovercraft.pdf
